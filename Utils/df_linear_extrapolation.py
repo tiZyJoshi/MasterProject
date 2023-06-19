7bfb1ba8c6c2a253b1e6db1ebc44df0e3a181426
tiZyJoshi <tiZyJoshi@users.noreply.github.com>
2023-06-19 02:17:59 +0200

    forecasting: arima

--- Utils/df_linear_extrapolation.py
+++ Utils/df_linear_extrapolation.py
@@ -24,3 +24,26 @@ class DFLinearExtrapolation(DFExtrapolation):
             ser[idx] = ser[idx].fillna(increment).cumsum()
             copy[col] = ser.array[::-1]
         return copy
+
+
+class DFSARIMAXExtrapolation(DFExtrapolation):
+    def run(self, df: pd.DataFrame) -> pd.DataFrame:
+        copy = df.copy()
+        for col in copy.columns:
+            # Extract column
+            ser = copy[col].copy()
+
+            # End piece
+            increment = ser.diff(1).mean()
+            idx_max_notna = ser[ser.notna()].index.array[-1]
+            idx = ser.index[ser.index >= idx_max_notna]
+            ser[idx] = ser[idx].fillna(increment).cumsum()
+
+            # Start piece
+            ser = pd.Series(ser.array[::-1])
+            increment = ser.diff(1).mean()
+            idx_max_notna = ser[ser.notna()].index.array[-1]
+            idx = ser.index[ser.index >= idx_max_notna]
+            ser[idx] = ser[idx].fillna(increment).cumsum()
+            copy[col] = ser.array[::-1]
+        return copy
